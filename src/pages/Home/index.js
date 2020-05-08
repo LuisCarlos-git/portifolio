58d5cd6b5bf43a2d37923ffe5f5746ade4741bb8
luis carlos <luisestrela.bol951@gmail.com>
2020-05-07 22:34:32 -0300

    finished home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,49 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Image, About, Box } from './styles';
+
+import perfil from '../../assets/perfil.jpg';
 
 export default function Home() {
-  return <Container />;
+  return (
+    <Container>
+      <Image>
+        <img src={perfil} alt="" />
+      </Image>
+
+      <About>
+        <Box>
+          <h1>Ai vai um resuminho sobre mim!</h1>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum
+            passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is
+            simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum
+            passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </Box>
+      </About>
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,73 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 30px auto;
+  width: 100%;
+  padding: 0 30px;
+`;
+export const About = styled.div`
+  position: relative;
+  width: 80%;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    right: -2px;
+    bottom: -2px;
+    background: #05fd01;
+    z-index: 1;
+    transform: skew(3deg, 3deg);
+  }
+`;
+
+export const Image = styled.div`
+  margin-right: 30px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 500px;
+    z-index: 2;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    right: -2px;
+    bottom: -2px;
+    background: #05fd01;
+    z-index: 1;
+    transform: skew(3deg, 3deg);
+  }
+`;
+export const Box = styled.div`
+  position: absolute;
+  z-index: 2;
+  background: #191920;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    color: #fff;
+    margin-bottom: 20px;
+    letter-spacing: 2px;
+  }
+
+  p {
+    color: #fff;
+    text-align: justify;
+    line-height: 20px;
+  }
+`;
