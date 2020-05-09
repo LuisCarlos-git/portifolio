9b2f0994b56295cb742f1867e8810817f26e78b9
luis carlos <luisestrela.bol951@gmail.com>
2020-05-08 23:50:09 -0300

    added page techs and information cards about techs

--- src/pages/Techs/index.js
+++ src/pages/Techs/index.js
@@ -1,7 +1,113 @@
 import React from 'react';
+import { FaReact, FaNodeJs, FaHtml5, FaCss3 } from 'react-icons/fa';
+import { DiJavascript1 } from 'react-icons/di';
 
-import { Container } from './styles';
+import {
+  Container,
+  List,
+  Title,
+  Description,
+  Content,
+  ListItem,
+  Icon,
+} from './styles';
 
 export default function Techs() {
-  return <Container />;
+  return (
+    <Container>
+      <List>
+        <ListItem title="react">
+          <Title>ReactJs</Title>
+          <Content>
+            <Description>
+              O React é uma biblioteca JavaScript de código aberto com foco em
+              criar interfaces de usuário em páginas web, apesar de ter apenas 6
+              meses de experiencia com a tecnologia ja desenvolvi bastante
+              projetos com a ferramenta.
+            </Description>
+            <Icon>
+              <FaReact size={50} color="#117ACB" />
+            </Icon>
+          </Content>
+        </ListItem>
+
+        <ListItem title="node">
+          <Title>Node.js</Title>
+          <Content>
+            <Description>
+              Node.js é um interpretador de JavaScript assíncrono com código
+              aberto orientado a eventos focado em migrar a programação do
+              Javascript do cliente para os servidores, Tenho bastante afinidade
+              com Node.js e me sinto confortavel em criar qualquer serviço com a
+              ferramenta, 06 meses de experiencia.
+            </Description>
+            <Icon>
+              <FaNodeJs size={50} color="green" />
+            </Icon>
+          </Content>
+        </ListItem>
+
+        <ListItem title="javascript">
+          <Title>JavaScript</Title>
+          <Content>
+            <Description>
+              JavaScript é uma linguagem de programação interpretada
+              estruturada, de script em alto nível com tipagem dinâmica fraca e
+              multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das
+              três principais tecnologias da World Wide Web, 1 ano de
+              experiencia.
+            </Description>
+            <Icon>
+              <DiJavascript1 size={50} color="yellow" />
+            </Icon>
+          </Content>
+        </ListItem>
+
+        <ListItem title="reactnative">
+          <Title>React Native</Title>
+          <Content>
+            <Description>
+              React Native é uma biblioteca Javascript criada pelo Facebook. É
+              usada para desenvolver aplicativos para os sistemas Android e IOS
+              de forma nativa, sempre me perguntava como eram feito os apps que
+              usamos no celular nessa busca encontrei o react native e apesar de
+              nao ter o total dominio da ferramenta estou na busca de me
+              aprimorar cada dia mais, 06 meses de experiencia.
+            </Description>
+            <Icon>
+              <FaReact size={50} color="#1144EA" />
+            </Icon>
+          </Content>
+        </ListItem>
+
+        <ListItem title="html">
+          <Title>HTML 5</Title>
+          <Content>
+            <Description>
+              HTML é uma linguagem de marcação utilizada na construção de
+              páginas na Web, meu primeiro contato com o desenvolvimento web, 1
+              ano de experiencia.
+            </Description>
+            <Icon>
+              <FaHtml5 size={50} color="#FD5001" />
+            </Icon>
+          </Content>
+        </ListItem>
+
+        <ListItem title="css">
+          <Title>CSS 3</Title>
+          <Content>
+            <Description>
+              Cascading Style Sheets é um mecanismo para adicionar estilo a um
+              documento web, a beleza encanta os olhos de quem a ve e com css
+              tudo fica mais estiloso, 1 ano de experiencia.
+            </Description>
+            <Icon>
+              <FaCss3 size={50} color="#0399EF" />
+            </Icon>
+          </Content>
+        </ListItem>
+      </List>
+    </Container>
+  );
 }
--- src/pages/Techs/styles.js
+++ src/pages/Techs/styles.js
@@ -1,3 +1,69 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+// eslint-disable-next-line consistent-return
+function borderColor(title) {
+  if (title === 'react') {
+    return '#117ACB';
+  }
+
+  if (title === 'node') {
+    return 'green';
+  }
+
+  if (title === 'javascript') {
+    return 'yellow';
+  }
+
+  if (title === 'reactnative') {
+    return '#1144EA';
+  }
+
+  if (title === 'html') {
+    return '#FD5001';
+  }
+
+  if (title === 'css') {
+    return '#0399EF';
+  }
+}
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+export const List = styled.ul`
+  width: 80%;
+  margin: 30px auto 100px;
+`;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+export const Title = styled.h2`
+  color: #fff;
+  width: 100%;
+`;
+export const Description = styled.p`
+  color: #fff;
+  width: 90%;
+`;
+
+export const ListItem = styled.li`
+  border-left: 3px solid ${({ title }) => borderColor(title)};
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+
+  & + li {
+    margin-top: 20px;
+  }
+`;
+export const Icon = styled.div`
+  padding: 10px;
+`;
